57e64e1c3e28597d560aba505ac80cd8c0168cfb
YogeshPatil31393 <yogesh.patil31393@gmail.com>
2021-05-16 18:51:49 +0530

    fix test

--- src/test/java/com/company/springjenkinsdockerapp/SpringJenkinsDockerAppApplicationTests.java
+++ src/test/java/com/company/springjenkinsdockerapp/SpringJenkinsDockerAppApplicationTests.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringJenkinsDockerAppApplicationTests {
 
 	@Autowired
@@ -22,7 +24,7 @@ class SpringJenkinsDockerAppApplicationTests {
 	@Test
 	public void testSampleController() throws Exception {
 		RequestBuilder builder = MockMvcRequestBuilders
-					.get("/sample/jenkinsdockerapp");
+					.get("/demo/jenkinsdockerapp");
 		
 		MvcResult result = mockMvc.perform(builder).andReturn();
 		
